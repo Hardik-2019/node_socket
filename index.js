debde47a366d115299dd64d26726b5cb76b1086f
Hardik Garg <hardikgarg01@gmail.com>
2021-10-21 02:06:50 +0530

    process env sorted

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ io.on('connection',(socket)=>{
     })
 })
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, ()=> console.log(`Server running at port number ${PORT}`))
